3c24b5bab01cb05746fde7265eff46140e417d53
Tolera Imiru <toleeraa2024@gmail.com>
2026-02-25 22:07:26 +0300

    Update app.py

--- scripting.py
+++ scripting.py
@@ -12,7 +12,6 @@ soup = BeautifulSoup(response.content, "html.parser")
 # Find the FIRST product link
 first_product = soup.find("a", class_="a-link-normal s-line-clamp-2 puis-line-clamp-3-for-col-4-and-8 s-link-style a-text-normal")
 
-
 # From that link, go inside h2 → span → text
 title = first_product.find("h2").text.strip()
 price_div = soup.find("div", attrs={"data-cy": "secondary-offer-recipe"})
